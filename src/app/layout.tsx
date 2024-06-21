f8be3f2cb5b20db72e400cb9fb7b1f501254fc98
iseabock <iseabock@gmail.com>
2024-06-20 20:22:53 -0700

    fix mobile background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en' className={fonts.cormorant.variable}>
       <body>
+        <div className="bg"></div>
         {children}
       </body>
     </html>
